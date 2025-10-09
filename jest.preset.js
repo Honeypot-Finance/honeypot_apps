f68c1002d9753fcede9b4658cb884548e49faa93
Shahzaib <shahzaibsheikh355@gmail.com>
2025-10-09 20:37:17 +0500

    testing trigger-1

--- jest.preset.js
+++ jest.preset.js
@@ -1,9 +1,4 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { 
-  ...nxPreset,
-  // Add global configuration for handling ES modules
-  transformIgnorePatterns: [
-    'node_modules/(?!(uuid|superjson|@solana/web3\\.js|@coral-xyz/anchor|@particle-network/.*|jayson|@rainbow-me/.*|wagmi|@wagmi/.*|@gemini-wallet/.*|viem|cuer|qr))',
-  ],
-};
+module.exports = { ...nxPreset };
+
